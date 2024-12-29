8082fa0a6203e95d24cf02133612a41ab93a73b6
josephrobinsonsimon@gmail.com <josephrobinsonsimon@gmail.com>
2024-12-29 04:47:23 +0000

    Resolved merge conflicts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import viteReactPlugin from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import viteConfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   build: { reportCompressedSize: false },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
   plugins: [
     viteConfigPaths(),
     viteReactPlugin(),
